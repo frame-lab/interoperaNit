56039d2a85f2a27ae5055d2f35783d55744333f7
Allan <luminussora@gmail.com>
2021-09-08 09:52:59 -0300

    added align to ontology ident

--- ontology ident/ontology/align.py
+++ ontology ident/ontology/align.py
@@ -6,9 +6,32 @@ class Align:
     def __init__(self, ontos):
         self.ontos = ontos
 
-    def make_print(self, result):
-        file = open(f'./results/ontology_result.txt', "w+")
-        file.writelines(str(result))
+    def make_files(self, domains):
+        for domain in domains:
+            file = open(f'./results/{domain["domain_name"]}.sql', "w+")
+
+            file.write(
+                f'CREATE TABLE IF NOT EXISTS "{domain["domain_name"]}" (\n')
+
+            for parameter in domain["domain_parameters"]:
+                separator = " "
+                file.write(
+                    f'\t"{parameter["parameter"]}" {separator.join(parameter["type"])},\n')
+
+            file.write(");\n\n")
+
+            separator = ", "
+            parameters = [f'"{parameter["parameter"]}"'
+                          for parameter in domain["domain_parameters"]]
+            file.write(
+                f'INSERT INTO "{domain["domain_name"]}" ({separator.join(parameters)}) VALUES\n')
+
+            last_entity = domain["entities"][-1]
+            for entity in domain["entities"]:
+                file.write(
+                    f'\t({separator.join(entity)}),\n')
+
+            file.write(f'\t({separator.join(last_entity)});\n')
 
     def make_domains(self):
         domains = []
@@ -20,7 +43,7 @@ class Align:
 
             domain = {
                 "domain_name": "",
-                "domain_parameter": [],
+                "domain_parameters": [],
                 "entities": [],
             }
 
@@ -36,23 +59,40 @@ class Align:
 
                 elif new_domain:
                     words = line.split(' ')
-                    domain["domain_parameter"].append(
-                        words[0].replace("\"", "").replace("\t", ""))
-
-                elif new_entities and f'INSERT INTO \"${domain["domain_name"]}\"':
-                    is_entities = True
-                    new_entities = False
+                    domain_parameter = {
+                        "parameter": words.pop(0).
+                        replace("\t", "").
+                        replace('"', ""),
+                        "type": [word.
+                                 replace(",", "").
+                                 replace("\n", "") for word in words]
+                    }
+                    domain["domain_parameters"].append(domain_parameter)
 
                 elif ";" in line and is_entities:
                     is_entities = False
+                    domains.append(domain)
+                    domain = {
+                        "domain_name": "",
+                        "domain_parameters": [],
+                        "entities": [],
+                    }
+
+                elif new_entities and f'INSERT INTO \"{domain["domain_name"]}\"' in line:
+                    is_entities = True
+                    new_entities = False
 
                 elif is_entities:
                     words = line.split(", ")
-                    domain["entities"].append(words)
-
-            domains.append(domain)
-            print(domain)
-
+                    entity = [word.
+                              replace("(", "").
+                              replace("\t", "").
+                              replace("),\n", "").
+                              replace("'", "") for word in words]
+                    domain["entities"].append(entity)
+        return domains
 
     def align(self):
-        self.make_domains()
+        domains = self.make_domains()
+
+        self.make_files(domains)
