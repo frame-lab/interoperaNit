6c6c749a4d00039e9c0b175cb006f145c7595b85
flavinha <luminussora@gmail.com>
2022-03-16 03:48:28 -0300

    removed test

--- interopera/test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-bigbase = pd.read_csv('samples/familia_logradouro.csv',
-                      encoding='utf8',
-                      quotechar='"',
-                      sep=',',
-                      dtype=object)
-print(bigbase.loc[:, "nome"][0])
