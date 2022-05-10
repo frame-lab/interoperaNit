125753fe494a534b0bf082083b70472347869b0e
Allan asus <luminussora@gmail.com>
2022-05-10 03:32:30 -0300

    added option to search sql query in the alignment

--- interopera/loading.py
+++ interopera/loading.py
@@ -10,14 +10,11 @@ query_base = ''
 def pandaBoolean(query):
     global query_base
     query_base = bigbase.query(query)
-    query_base.head()
 
 
 def pandaSQL(query):
     global query_base
     query_base = psql.sqldf(query)
-    query_base.head()
-    query_base.to_csv(f'results/query.csv', index=False)
 
 
 def export2CSV(path):
